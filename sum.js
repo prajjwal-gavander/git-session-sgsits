ce50e30a6249730255d2d0dc97d9d38d7ad1ca69
dell <prajjwal.gavander.2029@gmail.com>
2023-10-14 13:18:29 +0530

    feat: started project

---
+++ sum.js
@@ -0,0 +1,10 @@
+const Hi = 'Hello guys';
+const sessionStarted = 'Indhanpay Git session';
+
+const a = 10; // declare and initialize variable
+const b = 20; // declare and initialize variable
+
+// define sum function
+function sum() {
+  return a+b;
+}
